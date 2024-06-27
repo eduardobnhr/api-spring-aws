4df641e893947b8bf3b219a588a8a003570abf8e
eduardobnhr <eduardobnhr@gmail.com>
2024-06-27 11:11:50 -0400

    first commit

---
+++ src/main/java/br/com/events/api/domain/address/Address.java
@@ -0,0 +1,32 @@
+package br.com.events.api.domain.address;
+import java.util.Date;
+import java.util.UUID;
+
+import br.com.events.api.domain.eventos.Event;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String city;
+    private String uf;
+    
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
